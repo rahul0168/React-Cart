43825aaf150841feb521794a220037fb6470deba
rahul0168 <rahul.naikmule.facebook@gmail.com>
2023-02-12 17:22:37 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-
+import Navbar from './Navbar';
 import Cart from './Cart';
 
 function App() {
   return (
     <div className="App">
-     <h1>Cart PRoject</h1>
-    
+    <Navbar />    
     <Cart/>
     </div>
   );
